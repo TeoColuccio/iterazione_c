78d2ba1ea2f1f180ee81e0a0b5608a0490bda632
TeoColuccio <teodoro.coluccio@gmail.com>
2019-07-24 19:19:28 +0200

    Aggiunto massimo.c

---
+++ massimo.c
@@ -0,0 +1,25 @@
+/* Il programma accetta 10 numeri in input 
+ * e ne determina il maggiore
+ */
+
+#include <stdio.h>
+
+int main() {
+  int num, ma, max;
+  int i=2;
+
+  printf("Inserisci 10 numeri...\n");
+
+  printf("Numero 1: ");
+  scanf("%d", &num);
+  max=num;
+  while (i <= 10) {
+    printf("Numero %d: ", i);
+    scanf("%d", &num);
+    if (num>max) max = num;
+    i++;
+  }
+
+  printf("Il numero maggiore e': %d\n", max);
+  return 0;
+}
