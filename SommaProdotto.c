6d8940a3f3c4f9938e598b8ae6789ad99d2569db
TeoColuccio <teodoro.coluccio@gmail.com>
2019-08-31 18:13:55 +0200

    Rinominate le variabili

--- SommaProdotto.c
+++ SommaProdotto.c
@@ -6,16 +6,16 @@
 #include <stdio.h>
 
 int main() {
-  int i, SommaPari, SommaDispari;
+  int i, somma, prodotto;
 
-  SommaPari = 0;
-  for(i = 2; i <= 30; i+=2) SommaPari += i;
+  somma = 0;
+  for(i = 2; i <= 30; i+=2) somma += i;
   
-  SommaDispari = 1;
-  for(i = 1; i <= 15; i+=2) SommaDispari *= i;
+  prodotto = 1;
+  for(i = 1; i <= 15; i+=2) prodotto *= i;
 
-  printf("La somma dei numeri pari compresi tra 2 e 30 e': %d\n", SommaPari);
-  printf("Il prodotto dei numeri dispari compresi tra 1 e 15 e': %d\n", SommaDispari);
+  printf("La somma dei numeri pari compresi tra 2 e 30 e': %d\n", somma);
+  printf("Il prodotto dei numeri dispari compresi tra 1 e 15 e': %d\n", prodotto);
 
   return 0;
 }
