e8a4a95a01efc65a659dba71bff39cff319c6502
TeoColuccio <teodoro.coluccio@gmail.com>
2020-04-07 12:40:08 +0200

    Aggiunta la descrizione a spazimultipli.c

--- spazimultipli.c
+++ spazimultipli.c
@@ -1,3 +1,17 @@
+/* Il programma elimina gli spazi multipli in un file di testo in input
+ * 
+ * Stati possibili: 
+ * 0: c e' diverso dal carattere ' '
+ * 1: e' stato rilevato il carattere ' ', verifico se questo si ripete
+ * 
+ * Stato      Input                 Output      Nuovo stato
+ *   0       c != ' '                  c              - 
+ *   0       c == ' '                  -              1
+ *   1       c != ' '              ' ' + c            0
+ *   1       c == ' '                  -              - 
+ *   
+ */
+
 #include <stdio.h>
 
 int main() {
