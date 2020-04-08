52ca1fb7ee8dc693177a769be15d6f80a8cb131c
TeoColuccio <teodoro.coluccio@gmail.com>
2020-04-09 00:55:23 +0200

    stati definiti con enum

--- spazimultipli.c
+++ spazimultipli.c
@@ -1,32 +1,34 @@
 /* Il programma elimina gli spazi multipli in un file di testo in input
  * 
  * Stati possibili: 
- * 0: c e' diverso dal carattere ' '
- * 1: e' stato rilevato il carattere ' ', verifico se questo si ripete
+ * NORM: c e' diverso dal carattere ' '
+ * SPACE: e' stato rilevato il carattere ' ', verifico se questo si ripete
  * 
  * Stato      Input                 Output      Nuovo stato
- *   0       c != ' '                  c              - 
- *   0       c == ' '                  -              1
- *   1       c != ' '              ' ' + c            0
- *   1       c == ' '                  -              - 
+ *  NORM     c != ' '                  c              - 
+ *  NORM     c == ' '                  -            SPACE
+ *  SPACE    c != ' '              ' ' + c          NORM
+ *  SPACE    c == ' '                  -              - 
  *   
  */
 
 #include <stdio.h>
 
 int main() {
-    int stato = 0;
+    enum Stato { NORM, SPACE };
+
+    int stato = NORM;
     int c;
 
     while ( (c = getchar()) != EOF) {
-        if (stato == 0) {   
-            if(c == ' ') stato = 1;
+        if (stato == NORM) {   
+            if(c == ' ') stato = SPACE;
             putchar(c);
         }
-        else if (stato == 1) {
+        else if (stato == SPACE) {
             if(c != ' ') {
                 putchar(c);
-                stato = 0;
+                stato = NORM;
             }
         }
     }
